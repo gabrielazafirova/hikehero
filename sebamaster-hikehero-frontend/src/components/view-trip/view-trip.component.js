cf798c0d53ad150af2ec834ee25f55c474b6738a
Manuel Ströhmer <manuelstrohmer@Manuels-MacBook-Pro.local>
2017-07-14 23:28:57 +0200

    Improved use case book a trip

--- sebamaster-hikehero-frontend/src/components/view-trip/view-trip.component.js
+++ sebamaster-hikehero-frontend/src/components/view-trip/view-trip.component.js
@@ -36,6 +36,35 @@ class ViewTripComponentController{
 
     $onInit() {
         this.displaySlider();
+        this.makeButtonBold('overview-button');
+    }
+
+    showValidationMessages() {
+        this.clearValidationMessages();
+        this.showValidationMessage("card-first-name");
+        this.showValidationMessage("card-last-name");
+        this.showValidationMessage("card-type");
+        this.showValidationMessage("card-number");
+        this.showValidationMessage("card-approval-number");
+        this.showValidationMessage("card-valid-until-month");
+        this.showValidationMessage("card-valid-until-year");
+    }
+
+    showValidationMessage(id) {
+        var section = document.getElementById(id);
+        var input = section.getElementsByClassName("user-input")[0];
+        if (!input.checkValidity()) {
+            var validationMessage = section.getElementsByClassName("validation-message")[0];
+            validationMessage.innerHTML = input.validationMessage;
+            validationMessage.style.display = "block";
+        }
+    }
+
+    clearValidationMessages() {
+        var validationMessages = document.getElementsByClassName("validation-message");
+        for (var i = 0; i < validationMessages.length; i++) {
+            validationMessages[i].style.display = "none";
+        }
     }
 
     displaySlider() {
@@ -45,8 +74,7 @@ class ViewTripComponentController{
         // Go back to the end
         if (this.slideIndex < 1) {this.slideIndex = slides.length}
         // Set all slides invisible
-        var i;
-        for (i = 0; i < slides.length; i++) {
+        for (var i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
         // Only set the slide with the current index visible
@@ -64,8 +92,13 @@ class ViewTripComponentController{
     }
 
     openPaymentBox() {
-        let modal = document.getElementById('paymentBox');
-        modal.style.display = "block";
+        if (!this.isAuthenticated()) {
+            this.openNotLoggedInBox()
+        } else {
+            this.clearValidationMessages();
+            let modal = document.getElementById('paymentBox');
+            modal.style.display = "block";
+        }
     };
 
     closePaymentBox() {
@@ -84,6 +117,16 @@ class ViewTripComponentController{
         modal.style.display = "none";
     };
 
+    openNotLoggedInBox() {
+        let modal = document.getElementById('not-logged-in-box');
+        modal.style.display = "block";
+    };
+
+    closeNotLoggedInBox() {
+        let modal = document.getElementById('not-logged-in-box');
+        modal.style.display = "none";
+    }
+
     saveBooking() {
         let user = this.UserService.getCurrentUser();
         this.booking['trip'] = this.trip['_id'];
@@ -94,20 +137,40 @@ class ViewTripComponentController{
     };
 
     openOverview() {
+        this.normalizeFontWeightOfTabButtons();
+        this.makeButtonBold('overview-button');
         this.tabState = 0;
     };
 
     openGuide() {
+        this.normalizeFontWeightOfTabButtons();
+        this.makeButtonBold('guide-button');
         this.tabState = 1;
     };
 
     openLocation() {
+        this.normalizeFontWeightOfTabButtons();
+        this.makeButtonBold('location-button');
         this.tabState = 2;
     };
 
     openQuestions() {
+        this.normalizeFontWeightOfTabButtons();
+        this.makeButtonBold('questions-button');
         this.tabState = 3;
     };
+
+    makeButtonBold(id) {
+        let button = document.getElementById(id);
+        button.style.fontWeight = "bold";
+    }
+
+    normalizeFontWeightOfTabButtons() {
+        var tabButtons = document.getElementsByClassName("tab-buttons");
+        for (var i = 0; i < tabButtons.length; i++) {
+            tabButtons[i].style.fontWeight = "normal";
+        }
+    }
 
     edit () {
 
@@ -136,6 +199,10 @@ class ViewTripComponentController{
     isAuthenticated(){
         return this.UserService.isAuthenticated();
     };
+
+    login(){
+        this.$state.go('login',{});
+    }
 
     isTripCreator() {
         return this.UserService.getCurrentUser()._id == this.trip.user;
